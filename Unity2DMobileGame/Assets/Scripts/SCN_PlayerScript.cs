bbb491567f43dda644a335463c39da59bcd1abbd
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-09-21 22:05:57 +0100

    Update - Added in a player ball, and the ability to set the player rolling. Also, individual rotations for the pipes have been implemented, although the collider box seems to be smaller for mouse clicks.

---
+++ Unity2DMobileGame/Assets/Scripts/SCN_PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCN_PlayerScript : MonoBehaviour {
+
+	[Range(50.0f, 200.0f)][SerializeField] private float speed = 100.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Go()
+	{
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0.0f));
+	}
+}
---
+++ Unity2DMobileGame/Assets/Scripts/SCN_SingleRotaion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCN_SingleRotaion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown()
+	{
+		transform.parent.transform.Rotate(0.0f, 0.0f, 90.0f);
+	}
+}
