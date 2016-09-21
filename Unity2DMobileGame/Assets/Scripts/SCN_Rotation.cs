d8998a1c02058598a5fdc6cdcad8c9adc71beb2c
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-09-21 21:12:27 +0100

    Update - Implemented Level Container rotation and buttons to rotate it left and right.

---
+++ Unity2DMobileGame/Assets/Scripts/SCN_Rotation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCN_Rotation : MonoBehaviour {
+
+	[SerializeField] private const float rotationSpeed = 5.0f;
+	private float direction = 0.0f;
+	private float rotationValue = 0.0f;
+	private bool isRotating = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isRotating)
+		{
+			transform.Rotate(new Vector3(0.0f, 0.0f, rotationSpeed * direction));
+			rotationValue += rotationSpeed;
+			if(rotationValue >= 90.0f)
+			{
+				isRotating = false;
+				rotationValue = 0.0f;
+			}
+		}
+	}
+
+	public void Rotate(float dir)
+	{
+		if(!isRotating)
+		{
+			isRotating = true;
+			direction = dir;
+		}
+	}
+}
