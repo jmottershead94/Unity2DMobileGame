558413d6fb325a1e58e0f0d172d7f98394f4f1b1
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-09-27 14:32:40 +0100

    Update - Created the speed pipe script, this can allow pipes to have fast, normal, slow or reverse speed modifiers,

---
+++ Unity2DMobileGame/Assets/Scripts/SCR_SpeedPipe.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCR_SpeedPipe : MonoBehaviour {
+
+	private enum PipeType
+	{
+		fast,
+		normal,
+		slow,
+		reverse
+	};
+	[SerializeField] PipeType type = PipeType.normal;
+	[SerializeField] [Range(0.0f, 4.0f)] float speedModifier = 1.0f;
+	private float velocity = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		switch(type)
+		{
+			case PipeType.fast:
+			{
+				velocity = 1.5f;
+
+				break;
+			}
+			case PipeType.normal:
+			{
+				velocity = 1.0f;
+
+				break;
+			}
+			case PipeType.slow:
+			{
+				velocity = 0.5f;
+
+				break;
+			}
+			case PipeType.reverse:
+			{
+				velocity = -1.0f;
+
+				break;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.tag == "Player" && type != PipeType.normal)
+		{
+			collider.attachedRigidbody.velocity *= (velocity * speedModifier);	
+
+			Debug.Log("Velocity = " + collider.attachedRigidbody.velocity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
