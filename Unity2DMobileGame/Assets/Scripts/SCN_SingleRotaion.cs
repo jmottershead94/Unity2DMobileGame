2ec45e01bc3605f97d4124f08e728f601cf17163
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-10-03 15:58:08 +0100

    Fix - Touch input is now working on individual pipes, we just moved the interactable child forward on the z-axis.

--- Unity2DMobileGame/Assets/Scripts/SCN_SingleRotaion.cs
+++ Unity2DMobileGame/Assets/Scripts/SCN_SingleRotaion.cs
@@ -12,6 +12,8 @@ public class SCN_SingleRotaion : MonoBehaviour {
 	// Update is called once per frame
 	void OnMouseDown()
 	{
+		Debug.Log("Tap detected.");
+
 		if(!SCR_GameVariables.IsLocked)
 		{
 			transform.parent.Rotate(0.0f, 0.0f, 90.0f);
